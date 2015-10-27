60714a66830668ee77374d9355d1a2b27dbc7ca7
Fluvio Lobo <flobo@ist.ucf.edu>
2015-10-26 20:35:02 -0400

    Function design to execute the recording from a selected input recording device

---
+++ audioRecord.m
@@ -0,0 +1,12 @@
+%
+%
+% Record Audio
+%
+% This function records audio from the input recording device selected by
+% the user
+
+% Fluvio L. Lobo Fenoglietto
+
+
+
+recObj = audiorecorder(Fs,nBits,nChannels,selectedInputDeviceID);
