b9ce2e1ae4078f67a2196c225c9b5751be309074
unknown <flobo@P3226-lenovo2.net.ist.ucf.edu>
2015-11-17 12:18:19 -0500

    Changed name of function (reducing caps) and added functionality for a time-based audio recording

--- audioRecord.m
+++ audioRecord.m
@@ -1,31 +1,16 @@
 %
+% Audio Record
 %
-% Record Audio
+% The following function uses the connected input audio device to record
+% audio
 %
-% This function records audio from the input recording device selected by
-% the user
-
 % Fluvio L. Lobo Fenoglietto
+%
 
-
-function [recObj] = audioRecord(Fs, nBits, nChannels, selectedInputDeviceID)
-
-    recObj = audiorecorder(Fs,nBits,nChannels,selectedInputDeviceID);
-    
-    
-    recordingdlg = questdlg('Would you like to begin recording?', ...
-                            'Start Recording', ...
-                            'Yes','No','Yes');
-    % Handle response
-    switch recordingdlg
-        case 'Yes'
-            
-            record(recObj);
-            
-            
-        case 'No'
-            break
-    end 
-    
-    
-end
+function [audiosignal] = audiorecord(audiorecobj, time)
+    % [1] Begin recording
+    disp('Begin recording...');
+    recordblocking(audiorecobj, time); % time in seconds
+    % [2] Store recording into double-precision array
+    audiosignal = getaudiodata(audiorecobj,'double');
+end % End of function
