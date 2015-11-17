54e0f2f66d63c7b263a6796267f6a37eafbf7332
unknown <flobo@P3226-lenovo2.net.ist.ucf.edu>
2015-11-17 11:13:38 -0500

    Created function to search for available audio recording devices within the computer

---
+++ audiorecsearch.m
@@ -0,0 +1,23 @@
+%
+% Audio Recording Device Search
+%
+% The following function searches for audio recording devices within
+% windows. Given that this function searches for 'recording' devices, the
+% program only looks for 'audio-input' devices. This function will be
+% automated in the near future
+%
+% Fluvio L. Lobo Fenoglietto
+%
+
+function [audiorecdev] = audiorecsearch()
+    % [1] Search for audio recording devices installed in the computer
+    disp('Searching for audio recording devices...'); % Warning message on command window or console
+    audiorecinfo = audiodevinfo;
+    % [2] Storing device information in structure array
+    Ndevices = length(audiorecinfo.input); % Number of audio input devices
+    for i = 1:Ndevices
+        audiorecdev.devices{i} = audiorecinfo.input(i).Name;
+        audiorecdev.id(i) = audiorecinfo.input(i).ID;
+    end % End of for-loop
+    disp('Audio recording devices found and stored within "audiorecdev"...');
+end % End of function
