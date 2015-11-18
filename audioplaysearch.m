509ae0438621c0ebd6d83221ec20360569886780
unknown <flobo@P3226-lenovo2.net.ist.ucf.edu>
2015-11-18 10:29:54 -0500

    Created function to search for connected playback devices

---
+++ audioplaysearch.m
@@ -0,0 +1,23 @@
+%
+% Audio Playback Device Search
+%
+% The following function searches for audio playback devices within
+% windows. Given that this function searches for 'playback' devices, the
+% program only looks for 'audio-output' devices. This function will be
+% automated in the near future
+%
+% Fluvio L. Lobo Fenoglietto
+%
+
+function [audioplaydev] = audioplaysearch()
+    % [1] Search for audio recording devices installed in the computer
+    disp('Searching for audio playback devices...'); % Warning message on command window or console
+    audioplayinfo = audiodevinfo;
+    % [2] Storing device information in structure array
+    Ndevices = length(audioplayinfo.input); % Number of audio input devices
+    for i = 1:Ndevices
+        audioplaydev.devices{i} = audioplayinfo.input(i).Name;
+        audioplaydev.id(i) = audioplayinfo.input(i).ID;
+    end % End of for-loop
+    disp('Audio recording devices found and stored within "audioplaydev"...');
+end % End of function
