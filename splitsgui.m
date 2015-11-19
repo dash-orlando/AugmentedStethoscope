323a355a209ce5ab9783c08b5f0fa4f6c3ec266d
Fluvio Lobo <flobo@ist.ucf.edu>
2015-11-19 17:04:19 -0500

    Added pushbuttons in the recording section and changed object names across the entire graphical user interface

--- splitsgui.m
+++ splitsgui.m
@@ -53,13 +53,13 @@ varargout{1} = handles.output;
 %
 % Bluetooth Device Search
 % The button executes the function "btsearch.m"
-function pushbutton1_Callback(hObject, ~, handles)
+function searchbtpush_Callback(hObject, ~, handles)
 
         [btdev] = btsearch(); % searches for available bluetooth devices
         
-        Ndev = length(btdev.devices); % determining number of devices found
-        outString = horzcat(num2str(Ndev),' devices were found'); % reporting number of devices on string
-        set(handles.text5, 'String', outString); % printing report string
+        Nbtdev = length(btdev.devices); % determining number of devices found
+        outString = horzcat(num2str(Nbtdev),' devices were found'); % reporting number of devices on string
+        set(handles.searchbtstatus, 'String', outString); % printing report string
     
     handles.btdev = btdev; % stores bluetooth device information into handles structure
     
@@ -68,14 +68,14 @@ guidata(hObject, handles);
 %
 % Bluetooth Device Select
 % The button executes the function "btselect.m"
-function pushbutton2_Callback(hObject, ~, handles)
+function selectbtpush_Callback(hObject, ~, handles)
 
     btdev = handles.btdev; % extracting bluetooth device info from handles structure
 
         [btdevsel] = btselect(btdev); % allows the user to select from the list of found devices
         
         outString = horzcat('User selected ',btdevsel.device{1}); % reporting user selection
-        set(handles.text6, 'String', outString); % printing report string 
+        set(handles.selectbtstatus, 'String', outString); % printing report string 
     
     handles.btdevsel = btdevsel; % stores selected bluetooth device info within handles structure
 
@@ -84,18 +84,18 @@ guidata(hObject, handles);
 %
 % Bluetooth Device Connect
 % The button executes the function "btconnect.m"
-function pushbutton3_Callback(hObject, ~, handles)
+function connectbtpush_Callback(hObject, ~, handles)
 
     btdevsel = handles.btdevsel; % extracting selected bluetooth device info from handles structure
 
         [btobj] = btconnect(btdevsel); % creates a bluetooth object from the selected bluetooth device
         
         outString = horzcat('Computer connected to ',btdevsel.device{1}); % reporting connection
-        set(handles.text7, 'String', outString); % printing report string
+        set(handles.connectbtstatus, 'String', outString); % printing report string
         
-        set(handles.togglebutton1, 'Visible', 'on'); % set visibility of 'record' toggle button to 'on' 
-        set(handles.text8, 'Visible', 'on'); % set visibility of textbox toggle button to 'on' 
-        set(handles.togglebutton2, 'Visible', 'on'); % set visibility of 'playback' toggle button to 'on'
+        set(handles.recordtoggle, 'Visible', 'on'); % set visibility of 'record' toggle button to 'on' 
+        set(handles.or, 'Visible', 'on'); % set visibility of textbox toggle button to 'on' 
+        set(handles.playbacktoggle, 'Visible', 'on'); % set visibility of 'playback' toggle button to 'on'
     
     handles.btobj = btobj; % stores bluetooth object into handles structure
 
@@ -104,17 +104,17 @@ guidata(hObject, handles);
 %
 % Recording
 % The toggle button will activate the recording functions
-function togglebutton1_Callback(hObject, ~, handles)
+function recordtoggle_Callback(hObject, ~, handles)
 
     % import variables
     
         % functionality
-        set(handles.togglebutton2, 'Value', 0); % set the playback toggle button to 'off'
-        set(handles.pushbutton5, 'Visible', 'off'); % set playback button visibility 'off'
-        set(handles.text10, 'Visible', 'on'); % set playback title box visibility 'off'
+        set(handles.playbacktoggle, 'Value', 0); % set the playback toggle button to 'off'
+        set(handles.searchplaypush, 'Visible', 'off'); % set playback button visibility 'off'
+        set(handles.playtitle, 'Visible', 'on'); % set playback title box visibility 'off'
         
-        set(handles.text9, 'Visible', 'on'); % set record title box visibility 'on'
-        set(handles.pushbutton4, 'Visible', 'on'); % set record button visibility 'on'
+        set(handles.rectitle, 'Visible', 'on'); % set record title box visibility 'on'
+        set(handles.searchrecpush, 'Visible', 'on'); % set record button visibility 'on'
     
 
 guidata(hObject, handles);
@@ -122,31 +122,62 @@ guidata(hObject, handles);
 %
 % Playback
 % The toggle button will activate the playback functions
-function togglebutton2_Callback(hObject, ~, handles)
+function playbacktoggle_Callback(hObject, ~, handles)
 
     % import variables
     
         % functionality
-        set(handles.togglebutton1, 'Value', 0); % set the record toggle button to 'off'
-        set(handles.pushbutton4, 'Visible', 'off'); % set record button visibility 'off'
-        set(handles.text9, 'Visible', 'off'); % set record title box visibility 'off'
+        set(handles.recordtoggle, 'Value', 0); % set the record toggle button to 'off'
+        set(handles.searchrecpush, 'Visible', 'off'); % set record button visibility 'off'
+        set(handles.rectitle, 'Visible', 'off'); % set record title box visibility 'off'
         
-        set(handles.text10, 'Visible', 'on'); % set playback title box visibility 'on'
-        set(handles.pushbutton5, 'Visible', 'on'); % set playback button visibility 'on'
+        set(handles.playtitle, 'Visible', 'on'); % set playback title box visibility 'on'
+        set(handles.searchplaypush, 'Visible', 'on'); % set playback button visibility 'on'
          
 
 guidata(hObject, handles);
 
 
-% --- Executes on button press in pushbutton4.
-function pushbutton4_Callback(hObject, eventdata, handles)
-% hObject    handle to pushbutton4 (see GCBO)
+%
+% Audio Input/Recording Device Search
+% The button executes the function "audiorecsearch.m"
+function searchrecpush_Callback(hObject, ~, handles)
+
+    % import data from handles
+    
+        % executions
+        [audiorecdev] = audiorecsearch();
+        
+        Nrecdev = length(audiorecdev.devices); % determining number of devices found
+        outString = horzcat(num2str(Nrecdev),' devices were found'); % reporting number of devices on string
+        set(handles.searchrecstatus, 'String', outString); % printing report string
+        
+    % export to handles
+    handles.audiorecdev = audiorecdev;
+
+guidata(hObject, handles);
+
+%
+% Audio Input/Recording Device Selection
+% The button executes the function "audiorecselect.m"
+function selectrecpush_Callback(hObject, ~, handles)
+
+    audiorecdev = handles.audiorecdev; % extracting bluetooth device info from handles structure
+
+        [audiorecsel] = audiorecselect(audiorecdev); % allows the user to select from the list of found devices
+        
+        outString = horzcat('User selected ',audiorecsel.device{1}); % reporting user selection
+        set(handles.selectrecstatus, 'String', outString); % printing report string 
+    
+    handles.audiorecsel = audiorecsel; % stores selected bluetooth device info within handles structure
+
+guidata(hObject, handles);
+
+
+% --- Executes on button press in searchplaypush.
+function searchplaypush_Callback(hObject, eventdata, handles)
+% hObject    handle to searchplaypush (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
 
-% --- Executes on button press in pushbutton5.
-function pushbutton5_Callback(hObject, eventdata, handles)
-% hObject    handle to pushbutton5 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
