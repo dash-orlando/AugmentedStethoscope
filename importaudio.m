12bbb27435fd93ec7859ec94faba8e23c4864d95
Fluvio Lobo <flobo@ist.ucf.edu>
2015-11-26 17:50:58 -0500

    Created conditional loop to account for the case in which the user cancels/exits the file browser

--- importaudio.m
+++ importaudio.m
@@ -14,24 +14,28 @@
 function [audiodata] = importaudio()
 
     % the function sends a notification to the command window referring to the supported file extensions
-    disp('importaudio supports the following file extensions :: ');
+    % disp('importaudio supports the following file extensions :: ');
     supportedextensions = {'*.wav';'*.mp3';'*.mp4';'*.m4a';'*.ogg';'*.flac';'*.au';'*.aiff';'*.aif';'*.aifc'};
     
     % using uigetfile, the user is prompted to browse for the desired audio file
     [filename, pathname] = uigetfile(supportedextensions,'Select Audio File');
-    
-    % audioread is then used to convert the audio file into a vector composed of amplitude or voltage values
-    % using audioread, the sampling frequency of the selected audio file is determined
-    [signal, fs] = audioread(fullfile(pathname,filename));
-    
-    % the length of the signal vector and the sampling frequency are then used to calculate the time vector corresponding the signal 
-    tmax = length(signal)/fs;
-    time = 0:1/fs:tmax-1/fs;
-    
-    audiodata.audiopath = pathname; % pathname of the audio file
-    audiodata.audiofilename = filename; % name of the audio file
-    audiodata.time = time; % time array of the audio file
-    audiodata.signal = signal; % voltage or amplitude array of the audio file
-    audiodata.fs = fs; % sampling frequency
-    
+
+    if filename == 0        
+        disp('User did not select an audio file');
+        audiodata.audiofilename = 0;        
+    else       
+        % audioread is then used to convert the audio file into a vector composed of amplitude or voltage values
+        % using audioread, the sampling frequency of the selected audio file is determined
+        [signal, fs] = audioread(fullfile(pathname,filename));
+
+        % the length of the signal vector and the sampling frequency are then used to calculate the time vector corresponding the signal 
+        tmax = length(signal)/fs;
+        time = 0:1/fs:tmax-1/fs;
+
+        audiodata.audiopath = pathname; % pathname of the audio file
+        audiodata.audiofilename = filename; % name of the audio file
+        audiodata.time = time; % time array of the audio file
+        audiodata.signal = signal; % voltage or amplitude array of the audio file
+        audiodata.fs = fs; % sampling frequency        
+    end   
 end
