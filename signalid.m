5803d9046bcaa08164f8bacf54ee036a26881a07
Fluvio Lobo <flobo@ist.ucf.edu>
2015-11-29 22:26:47 -0500

    Shifted sections, change variable names, and added comments to the function

--- signalid.m
+++ signalid.m
@@ -9,40 +9,59 @@
 
 function [excision] = signalid(audiodata,extract,conv)
 
-    x = audiodata.signal;
-    y = s1.signal;
-    conv = 0.96;
+    signal = audiodata.signal;
+    extract = extract.signal;
+    % conv = 0.96;
 
     % vector lengths
-    lenx = length(x);
-    leny = length(y);
+    Nsignal = length(signal);
+    Nextract = length(extract);
 
     % signal autocorrelation
-    aC = 0;
-    for i = 1:leny
-        aC = aC + y(i)*y(i);
-    end
+    % the autocorrelation of the extract is used to calculate a
+    % normalization coefficient
+    aCorr = 0;
+    for i = 1:Nextract
+        aCorr = aCorr + extract(i)*extract(i);
+    end % end of for-loop
 
     % signal cross-correlation
-    C = zeros(lenx-leny,1);
-    %nC = zeros(lenx-leny,1);
-    for i = 1:lenx-leny  
-        for j = 1:leny       
-            C(i) = C(i) + x(i+j-1)*y(j);       
+    % the "cross-correlation" routine below does not follow the exact
+    % mathematical formulation of cross-correlation. In general, the
+    % extract (the shorter signal) is sweeped through the signal, starting
+    % and finishing with a complete overlay of the extract.
+    cCorr = zeros(Nsignal-Nextract,1);
+    for i = 1:Nsignal-Nextract  
+        for j = 1:Nextract       
+            cCorr(i) = cCorr(i) + signal(i+j-1)*extract(j);       
         end
         % cross correlation normalization
-        nC(i) = C(i)/aC;
-        if nC(i) > conv
-            nCp(i) = nC(i);
+        % the auto-correlation coefficient is used to normalize the
+        % comparison to 1, allowing the user to specify an arbitrary
+        % convergence tolerance parameter.
+        nCorr(i) = cCorr(i)/aCorr;
+        if nCorr(i) > conv
             break
-        else
-            nCp(i) = 0;
-        end
-    end
+        end % end of if-statement
+    end % end of for-loop
 
     % excising region of correlation
-    corrindex = find(nC > conv);
-
-    excisedx = x(corrindex:corrindex+leny-1);
+    corrindex = find(nCorr > conv);
+    excision = signal(corrindex:corrindex+Nextract-1);
     
+    % create dummy array for visualization
+    nCplot = zeros(Nsignal,1);
+    for i = corrindex:corrindex+Nextract-1
+        nCplot(i) = excision(i-corrindex+1,1);
+    end
+    
+    % plotting signal match
+    figure,
+    plot(signal,'b')
+    hold on
+    plot(nCplot,'r')
+    xlabel('Time (sec.)');
+    ylabel('Amplitude (V)');
+    legend('Signal','Extract');
+   
 end % End of function
