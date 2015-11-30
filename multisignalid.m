367492fe963674d2e4001e768135f8bda2edff5a
unknown <flobo@P3226-lenovo2.net.ist.ucf.edu>
2015-11-30 11:46:18 -0500

    Converted script into function, enclosed plotting routine within a visualization input command (character input and string comparison check) , and added comments

--- multisignalid.m
+++ multisignalid.m
@@ -7,54 +7,57 @@
 % Fluvio L Lobo Fenoglietto
 %
 
-nmatch = 2;
-conv = 0.95;
+function [multiexcision] = multisignalid(audiodata,extract,conv,nmatch,vis)
 
-baseaudio = audiodata;
 
-for i = 1:nmatch
-    
-    exid = strcat('ex',num2str(i));
-    
-    [multiexcision.(exid)] = signalid(baseaudio,extract,conv);
-    
-    % deleting excision from signal
-    baseaudio.signal = baseaudio.signal - multiexcision.(exid).compsignal;
+    for i = 1:nmatch
 
-end
+        exid = strcat('ex',num2str(i));
 
-% plotting
-figure,
-subplot 211
-plot(audiodata.time,audiodata.signal,'b');
-hold on
-legendlabels = cell(nmatch+1,1);
-legendlabels(1,1) = {'Original Signal'}; 
-for i = 1:nmatch
-    exid = strcat('ex',num2str(i));
-    plot(multiexcision.(exid).comptime,multiexcision.(exid).compsignal,'Color',rand(1,3));
-    legendlabels(i+1,1) = {horzcat('Excision ',num2str(i))};
-end
-xlabel('Time (sec.)');
-ylabel('Amplitude (V)');
-title('Multiple Signal Identification');
-legend(legendlabels);
-grid on
+        [multiexcision.(exid)] = signalid(audiodata,extract,conv);
 
-subplot 212
-plot(audiodata.time,audiodata.signal,'b');
-hold on
-lasttimepoint = zeros(nmatch,1);
-legendlabels = cell(nmatch+1,1);
-legendlabels(1,1) = {'Original Signal'}; 
-for i = 1:nmatch
-    exid = strcat('ex',num2str(i));
-    plot(multiexcision.(exid).comptime,multiexcision.(exid).compsignal,'Color',rand(1,3));
-    lasttimepoint(i,1) = multiexcision.(exid).time(end);
-    legendlabels(i+1,1) = {horzcat('Excision ',num2str(i))};
-end
-xlabel('Time (sec.)');
-ylabel('Amplitude (V)');
-legend(legendlabels);
-axis([0 max(lasttimepoint)+1 min(audiodata.signal) max(audiodata.signal)]); 
-grid on
+        % deleting excision from signal
+        audiodata.signal = audiodata.signal - multiexcision.(exid).compsignal;
+
+    end % end of for-loop
+
+    if strcmp(vis,'on')
+        % plotting
+        figure,
+        
+        subplot 211
+            plot(audiodata.time,audiodata.signal,'b');
+            hold on
+            legendlabels = cell(nmatch+1,1);
+            legendlabels(1,1) = {'Original Signal'}; 
+            for i = 1:nmatch
+                exid = strcat('ex',num2str(i));
+                plot(multiexcision.(exid).comptime,multiexcision.(exid).compsignal,'Color',rand(1,3));
+                legendlabels(i+1,1) = {horzcat('Excision ',num2str(i))};
+            end
+            xlabel('Time (sec.)');
+            ylabel('Amplitude (V)');
+            title('Multiple Signal Identification');
+            legend(legendlabels);
+            grid on
+
+        subplot 212
+            plot(audiodata.time,audiodata.signal,'b');
+            hold on
+            lasttimepoint = zeros(nmatch,1);
+            legendlabels = cell(nmatch+1,1);
+            legendlabels(1,1) = {'Original Signal'}; 
+            for i = 1:nmatch
+                exid = strcat('ex',num2str(i));
+                plot(multiexcision.(exid).comptime,multiexcision.(exid).compsignal,'Color',rand(1,3));
+                lasttimepoint(i,1) = multiexcision.(exid).time(end);
+                legendlabels(i+1,1) = {horzcat('Excision ',num2str(i))};
+            end
+            xlabel('Time (sec.)');
+            ylabel('Amplitude (V)');
+            legend(legendlabels);
+            axis([0 max(lasttimepoint)+1 min(audiodata.signal) max(audiodata.signal)]); 
+            grid on
+    end % end of if-statement
+
+end % End of function
