01d023de6da6747f0a45093314f76d72223eef51
Fluvio Lobo <fluvio.lobo@gmail.com>
2016-08-30 11:08:19 -0400

    Added and modified two serial communication codes that will manage bluetooth communication on the teensy.

--- Software/Arduino/bluetoothDataRelay/bluetoothDataRelay.ino
+++ Software/Arduino/bluetoothDataRelay/bluetoothDataRelay.ino
@@ -5,18 +5,23 @@
  * 
  * Fluvio L Lobo Fenoglietoo 08/29/2016
  */
-#define HWSERIAL Serial2
+char INBYTE;
+int  LED = 13; // LED on pin 13
 
-int ledPin = 13;
+HardwareSerial bt = HardwareSerial();
 
 void setup() {
-  Serial.begin(115200);
-
+  pinMode(LED, OUTPUT);
+  
+  bt.begin(115200);
+  bt.println(); 
 }
 
 void loop() {
-
-  Serial2.print("hola");
-  Serial2.print("\n");
-  
+  bt.println("Press 1 to turn Arduino pin 13 LED ON or 0 to turn it OFF:");
+  //while (!bt.available());   // stay here so long as COM port is empty   
+  //INBYTE = bt.read();        // read next available byte
+  //if( INBYTE == '0' ) digitalWrite(LED, LOW);  // if it's a 0 (zero) tun LED off
+  //if( INBYTE == '1' ) digitalWrite(LED, HIGH); // if it's a 1 (one) turn LED on
+  //delay(50);
 }
