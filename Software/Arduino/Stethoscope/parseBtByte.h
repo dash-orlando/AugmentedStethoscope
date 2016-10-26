be5a74b4c1e06428ca1624aed62c290c5eab75c0
Fluvio Lobo <fluvio.lobo@gmail.com>
2016-10-26 16:19:10 -0400

    Adapted new modifications by MX.

--- Software/Arduino/Stethoscope/parseBtByte.h
+++ Software/Arduino/Stethoscope/parseBtByte.h
@@ -24,18 +24,12 @@ void loop()
 
 void displayByte( byte byteValue )
 {
-  blipRX();
   Serial.print( "received: [" );
   Serial.print( (char)byteValue );
   Serial.print( "][" );
   Serial.print( byteValue, HEX );
   Serial.println( ']' );
 }
-
-#define         DC1                 0x00      // temp definition, so that code will compile
-#define         DC2                 0x01      // temp definition, so that code will compile
-#define         DC3                 0x02      // temp definition, so that code will compile
-#define         DC4                 0x03      // temp definition, so that code will compile
 
 // Place functions here...
 
@@ -58,24 +52,17 @@ void parseBtByte( String fn )
     switch ( inByte )
     {
       case ENQ :
+        Serial.println( "received: ENQ..." );
         if ( readyState == READY )
         {
-          blipTX();
           connectState  = CONNECTED;
-          Serial.println( "received: ENQ" );
-          Serial.print( "sending: ENQ..." );
-          Serial.print( ENQ );
-          BTooth.write( ENQ ); 
-          Serial.print( "...done.\nsending: ACK..." );
-          Serial.print( ACK );
+          Serial.println( "sending: ACK..." );
           BTooth.write( ACK );
-          Serial.println( "...done." );
         }
         else
         {
-          BTooth.write( ENQ ); 
+          Serial.println( "sending: NAK..." ); 
           BTooth.write( NAK );
-          Serial.println( "sending: ENQ-NAK" );
         }
       break;
       case EOT :
@@ -169,158 +156,6 @@ void parseBtByte( String fn )
         }
       break;
 
-      //
-      //  *** Start RECORDing by remote command
-      //
-      case DC1_STRTREC :
-        if ( ( connectState == CONNECTED ) && ( recordState == STANDBY ) )
-        {
-          blipTX();
-          if ( startRecording() )
-          {
-            BTooth.write( DC1_STRTREC );
-            BTooth.write( ACK );
-            digitalWrite( REC, HIGH );
-            Serial.println( "REC-ACK" );
-//			continueRecording();
-			mode = 1;
-          }
-          else
-          {
-            BTooth.write( DC1_STRTREC );
-            BTooth.write( NAK );
-            Serial.println( "REC-NAK" );
-          }
-        }
-        else
-        {
-          BTooth.write( DC1_STRTREC );
-          BTooth.write( NAK );
-          Serial.println( "REC-NAK2" );
-        }
-      break;
-
-      //
-      //  *** Stop RECORDing by remote command
-      //
-      case DC2_STPREC :
-        if ( ( connectState == CONNECTED ) && ( recordState == RECORDING ) )
-        {
-          blipTX();
-          if ( stopRecording() )
-          {
-            BTooth.write( DC2_STPREC );
-            BTooth.write( ACK );
-            digitalWrite( REC, LOW );
-            Serial.println( "STPR-ACK" );
-			mode = 0;
-          }
-          else
-          {
-            BTooth.write( DC2_STPREC );
-            BTooth.write( NAK );
-            Serial.println( "STPR-NAK" );
-          }
-        }
-        else
-        {
-          BTooth.write( DC2_STPREC );
-          BTooth.write( NAK );
-          Serial.println( "STPR-NAK2" );
-        }
-      break;
-
-      //
-      //  *** Start PLAYing by remote command
-      //
-      case DC3_STRTPLY :
-        if ( ( connectState == CONNECTED ) && ( recordState == STANDBY ) )
-        {
-          blipTX();
-          if ( startPlaying() )
-          {
-            BTooth.write( DC3_STRTPLY );
-            BTooth.write( ACK );
-            flashLEDs( 1000, LOW, LOW );
-            Serial.println( "PLY-ACK" );
-	//		continuePlaying();
-			mode = 2;
-          }
-          else
-          {
-            BTooth.write( DC3_STRTPLY );
-            BTooth.write( NAK );
-            Serial.println( "PLY-NAK" );
-          }
-        }
-        else
-        {
-          BTooth.write( DC3_STRTPLY );
-          BTooth.write( NAK );
-          Serial.println( "PLY-NAK2" );
-        }
-      break;
-
-      //
-      //  *** Stop PLAYing by remote command
-      //
-      case DC4_STPPLY :
-        if ( ( connectState == CONNECTED ) && ( recordState == PLAYING ) )
-        {
-          blipTX();
-          if ( stopPlaying() )
-          {
-            BTooth.write( DC4_STPPLY );
-            BTooth.write( ACK );
-            stopLEDs();
-            Serial.println( "STPP-ACK" );
-			mode = 0;
-          }
-          else
-          {
-            BTooth.write( DC4_STPPLY );
-            BTooth.write( NAK );
-            Serial.println( "STPP-NAK" );
-          }
-        }
-        else
-        {
-          BTooth.write( DC4_STPPLY );
-          BTooth.write( NAK );
-          Serial.println( "STPP-NAK" );
-        }
-      break;
-      //
-      //  *** Delete 'RECORD.RAW' file from SD card
-      //
-      case ESC :
-        if ( connectState == CONNECTED )
-        {
-          blipTX();
-          if ( SD.exists( filename ) )
-          {
-            Serial.print( "Sending '" );
-            Serial.print( fn );
-            Serial.print( "'.\n" );
-            delay( 6000 );                                            // ...optional; in case time is needed to start host receive
-            sendFileSerial( SD.open( filename ) );                    // <--**** new function that sends WAV over Serial
-            BTooth.write( ESC );
-            BTooth.write( ACK );
-            Serial.println( "ESC-ACK" );
-          }
-          else
-          {
-            Serial.print( "'" );
-			      Serial.print( fn );
-			      Serial.print( "' does not exist.\n" );
-            BTooth.write( ESC );
-            BTooth.write( NAK );
-            Serial.println( "ESC-NAK" );
-          }
-          rootDir = SD.open( "/" );
-          printDirectory( rootDir, 1 );
-        }
-      break;
       default :
         Serial.print( (char)inByte );
       break;
--- Software/Arduino/Stethoscope/protocol.h
+++ Software/Arduino/Stethoscope/protocol.h
@@ -14,30 +14,32 @@
 #define         BTooth      Serial1
 
 /// ASCII Byte Codes -- used for communication protocol
-#define         CHK                 0x01          // System Check: "Run system check and report"                                [resp: --- | ---]
+// General Commands
 #define         ENQ                 0x05          // Enquiry: "Are you ready for commands?";                                    [resp: ACK | NAK]
 #define         EOT                 0x04          // End of Transmission: "Done; you may disconnect."                           [resp: ACK | NAK]
 #define         ACK                 0x06          // Positive Acknowledgement: "Command/Action successful."
 #define         NAK                 0x15          // Negative Acknowledgement: "Command/Action UNsuccessful."
 #define         CAN                 0x18          // Cancel: "Cancel current command/action.."                                  [resp: ACK | NAK]
-#define         ESC                 0x1B          // Escape: "Operate in alternate mode." (reserved for future use)
 
-#define         DC1                 0x11          // Device Command 1: Operation Functions                                      [resp: ACK | NAK]
-#define         DC1_CHK             0x01          //                   System Check: "Run system check and report"              [resp: ACK | NAK]
+// Device Control Commands
+#define         DC1                 0x11          // Device Control 1: Diagnostic Functions                                     [resp: ACK | NAK]
+#define         DC1_STATUSENQ       0x00          //                   Enquiry: "Are you ready for commands?";                  [resp: ACK | NAK]
+#define         DC1_SDCHECK         0x01          //                   System Check: "Run system check and report"              [resp: ACK | NAK]
+//                                  0xFF          (255 DEC)
 
-#define         DC2                 0x12          // Device Command 2: Communication Functions                                  [resp: ACK | NAK]
-#define         DC2_SNDWAV          0x01          //                   Send .WAV file (audio recording) via serial port         [resp: ACK | NAK]  
+#define         DC2                 0x12          // Device Control 2: Operational Functions                                    [resp: ACK | NAK]
+#define         DC2_SENDWAV         0x00          //                   Send .WAV file (audio recording) via serial port         [resp: ACK | NAK]
+#define         DC2_DELVOLATILE     0x01          //                   Erase volatile files (all)                               [resp: ACK | NAK]                     
+//                                  0xFF          (255 DEC)
 
-#define         DC3                 0x13          // Device Command 3: Device-Specific Functions                                [resp: ACK | NAK]
-#define         DC3_STRTREC         0x01          //                   Start Recording                                          [resp: ACK | NAK]
-#define         DC3_STPREC          0x02          //                   Stop Recording                                           [resp: ACK | NAK]
-#define         DC3_STRPLY          0x03          //                   Start Playback                                           [resp: ACK | NAK]
-#define         DC3_STPPLY          0x04          //                   Stop Playback                                            [resp: ACK | NAK]
+#define         DC3                 0x13          // Device Control 3: Device-Specific Functions                                [resp: ACK | NAK]
+#define         DC3_STARTREC        0x00          //                   Start Recording                                          [resp: ACK | NAK]
+#define         DC3_STOPREC         0x01          //                   Stop Recording                                           [resp: ACK | NAK]
+#define         DC3_STARTPLAY       0x02          //                   Start Playback                                           [resp: ACK | NAK]
+#define         DC3_STOPPLAY        0x03          //                   Stop Playback                                            [resp: ACK | NAK]
+//                                  0xFF          (255 DEC)
 
-#define         DC4                 0x14          // Device Command 4: Simulated Output                                         [resp: ACK | NAK]
-
-
-
+#define         DC4                 0x14          // Device Control 4: Simulation Functions                                     [resp: ACK | NAK]
 
 
 // Function Calls
