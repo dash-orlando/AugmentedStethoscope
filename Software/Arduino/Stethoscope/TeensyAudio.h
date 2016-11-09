0d0cc494323389a6acf2b9f41de0e175be95808c
Fluvio Lobo <fluvio.lobo@gmail.com>
2016-11-09 17:04:57 -0500

    Updated the teensy-audio parameters that control the microphone and audio outputs.

--- Software/Arduino/Stethoscope/TeensyAudio.h
+++ Software/Arduino/Stethoscope/TeensyAudio.h
@@ -3,23 +3,27 @@
 #include <SD.h>
 #include <Audio.h>
 
-// GUItool: begin automatically generated code
-AudioInputI2S            i2s2;           //xy=228.1999969482422,100
-AudioPlaySdRaw           playRaw1;       //xy=379.20001220703125,304
-AudioMixer4              mixer1;         //xy=392.20001220703125,115.19999694824219
-AudioOutputI2S           i2s1;           //xy=550.2000122070312,240.99998474121094
-AudioAnalyzePeak         peak1;          //xy=557.2000122070312,159.99998474121094
-AudioRecordQueue         queue1;         //xy=560.2000122070312,108
-AudioConnection          patchCord1(i2s2, 0, mixer1, 0);
-AudioConnection          patchCord2(i2s2, 1, mixer1, 1);
-AudioConnection          patchCord3(playRaw1, 0, i2s1, 0);
-AudioConnection          patchCord4(playRaw1, 0, i2s1, 1);
-AudioConnection          patchCord5(mixer1, peak1);
-AudioConnection          patchCord6(mixer1, queue1);
-AudioControlSGTL5000     sgtl5000_1;     //xy=361.20001220703125,376
-// GUItool: end automatically generated code
+const int        myInput    = AUDIO_INPUT_MIC;
 
-const int                myInput         = AUDIO_INPUT_MIC;
+// GUItool: begin automatically generated code
+AudioInputI2S            i2s2;                                    //xy=228.1999969482422,100
+AudioPlaySdRaw           playRaw1;                                //xy=230.1999969482422,211.99998474121094
+AudioMixer4              mixer1;                                  //xy=392.20001220703125,115.19999694824219
+AudioMixer4              mixer2;                                  //xy=393.20001220703125,198.1999969482422
+AudioOutputI2S           i2s1;                                    //xy=554.2000122070312,213.99998474121094
+AudioAnalyzePeak         peak1;                                   //xy=557.2000122070312,159.99998474121094
+AudioRecordQueue         queue1;                                  //xy=560.2000122070312,108
+AudioConnection          patchCord1( i2s2,      0, mixer1,  0 );
+AudioConnection          patchCord2( i2s2,      0, mixer2,  0 );
+AudioConnection          patchCord3( i2s2,      1, mixer1,  1 );
+AudioConnection          patchCord4( i2s2,      1, mixer2,  1 );
+AudioConnection          patchCord5( playRaw1,  0, mixer2,  2 );
+AudioConnection          patchCord6( mixer1,    peak1         );
+AudioConnection          patchCord7( mixer1,    queue1        );
+AudioConnection          patchCord8( mixer2,    0, i2s1,    0 );
+AudioConnection          patchCord9( mixer2,    0, i2s1,    1 );
+AudioControlSGTL5000     sgtl5000_1;                                //xy=233.1999969482422,268
+// GUItool: end automatically generated code
 
 void SetupAudioBoard()
 {
@@ -35,4 +39,8 @@ void SetupAudioBoard()
  // Configure SPI for the audio shield pins
   SPI.setMOSI( 7 );  // Audio shield has MOSI on pin 7
   SPI.setSCK( 14 );  // Audio shield has SCK on pin 14
+
+  mixer2.gain(0,0.0);
+  mixer2.gain(1,0.0);
+  
 }
