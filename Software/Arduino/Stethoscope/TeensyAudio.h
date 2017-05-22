8af8ca81f68dafed7c8745a3ad612f2ae58fc53e
Michael Xynidis <michael@xynidis.com>
2017-05-22 15:08:20 -0400

    High-shelf filter added to speaker output to attenuate resonate highs.

--- Software/Arduino/Stethoscope/TeensyAudio.h
+++ Software/Arduino/Stethoscope/TeensyAudio.h
@@ -15,13 +15,26 @@
 #include <SerialFlash.h>
 
 /// Variable Definitions
-const int                myInput          = AUDIO_INPUT_MIC;
-float                    mixer1ON         = 1.00;
-float                    mixer1OFF        = 0.00;
-float                    mixer2ON         = 1.00;
-float                    mixer2OFF        = 0.00;
-float                    mixerLvL         = 1.00;
-float                    cornerFreq       = 750.00;                            // Hz
+const int                 myInput         = AUDIO_INPUT_MIC;
+
+float                     mixer1ON        =    1.00;
+float                     mixer1OFF       =    0.00;
+float                     mixer2ON        =    1.00;
+float                     mixer2OFF       =    0.00;
+float                     mixerLvL        =    1.00;
+float                     cornerFreq      =  750.00;                            // Hz
+                          
+float                     freq1           =  3000.0f;                           // Speaker output, low-shelf filter, Stage 1
+float                     tGain1          =   -10.0f;
+float                     slope1          =     0.05f;
+
+float                     freq2           =  7700.0f;                           // Speaker output, low-shelf filter, Stage 2
+float                     tGain2          =   -60.0f;
+float                     slope2          =     0.05f;
+
+float                     freq3           = 15700.0f;                           // Speaker output, low-shelf filter, Stage 3
+float                     tGain3          =   -40.0f;
+float                     slope3          =     0.05f;
 
 /// Audio Shield Variables
 #include <Audio.h>
@@ -31,32 +44,35 @@ float                    cornerFreq       = 750.00;                            /
 #include <SerialFlash.h>
 
 // GUItool: begin automatically generated code
-AudioPlaySdRaw           playRaw1;       //xy=581.5999755859375,586
-AudioInputI2S            i2s2;           //xy=586.5999755859375,387
-AudioMixer4              mixer2;         //xy=861.5999755859375,535
-AudioMixer4              mixer1;         //xy=881.5999755859375,331
-AudioFilterStateVariable filter2;        //xy=1061.5999969482423,319.1999969482422
-AudioFilterStateVariable filter1;        //xy=1071.5999755859375,633
-AudioAnalyzePeak         peak2;          //xy=1241.5999755859375,705
-AudioRecordQueue         queue1;         //xy=1249.5999450683594,270.00000762939453
-AudioAnalyzePeak         peak1;          //xy=1256.5999450683594,366.00000762939453
-AudioOutputI2S           i2s1;           //xy=1263.5999755859375,622
-AudioRecordQueue         queue2;         //xy=1272.5999755859375,489
+AudioPlaySdRaw           playRaw1;       //xy=77.19999694824219,360
+AudioInputI2S            i2s2;           //xy=82.19999694824219,161
+AudioMixer4              mixer2;         //xy=357.1999969482422,309
+AudioMixer4              mixer1;         //xy=377.1999969482422,105
+AudioFilterStateVariable filter2;        //xy=557.1999969482422,93
+AudioFilterBiquad        biquad1;        //xy=560.2000122070312,283.20001220703125
+AudioFilterStateVariable filter1;        //xy=567.1999969482422,407
+AudioAnalyzePeak         peak2;          //xy=737.1999969482422,479
+AudioRecordQueue         queue1;         //xy=745.1999969482422,44
+AudioAnalyzePeak         peak1;          //xy=752.1999969482422,140
+AudioOutputI2S           i2s1;           //xy=759.1999969482422,396
+AudioRecordQueue         queue2;         //xy=768.1999969482422,263
 AudioConnection          patchCord1(playRaw1, 0, mixer2, 2);
 AudioConnection          patchCord2(i2s2, 0, mixer1, 0);
 AudioConnection          patchCord3(i2s2, 0, mixer2, 0);
 AudioConnection          patchCord4(i2s2, 1, mixer1, 1);
 AudioConnection          patchCord5(i2s2, 1, mixer2, 1);
-AudioConnection          patchCord6(mixer2, 0, filter1, 0);
+AudioConnection          patchCord6(mixer2, biquad1);
 AudioConnection          patchCord7(mixer1, 0, filter2, 0);
 AudioConnection          patchCord8(filter2, 0, queue1, 0);
 AudioConnection          patchCord9(filter2, 0, peak1, 0);
-AudioConnection          patchCord10(filter1, 0, i2s1, 0);
-AudioConnection          patchCord11(filter1, 0, i2s1, 1);
-AudioConnection          patchCord12(filter1, 0, queue2, 0);
-AudioConnection          patchCord13(filter1, 0, peak2, 0);
-AudioControlSGTL5000     sgtl5000_1;     //xy=752.5999755859375,687
+AudioConnection          patchCord10(biquad1, 0, filter1, 0);
+AudioConnection          patchCord11(filter1, 0, i2s1, 0);
+AudioConnection          patchCord12(filter1, 0, i2s1, 1);
+AudioConnection          patchCord13(filter1, 0, queue2, 0);
+AudioConnection          patchCord14(filter1, 0, peak2, 0);
+AudioControlSGTL5000     sgtl5000_1;     //xy=248.1999969482422,461
 // GUItool: end automatically generated code
+
 
 /// Functions
 void SetupAudioBoard()
@@ -80,6 +96,10 @@ void SetupAudioBoard()
   mixer2.gain(0,mixer2ON);                                                      // Set gain of mixer2, channel0 to 0.25 - Microphone on
   mixer2.gain(1,mixer2ON);                                                      // Set gain of mixer2, channel0 to 0.25 - Microphone on
   mixer2.gain(2,mixer2OFF);                                                     // Set gain of mixer2, channel2 to 0
+
+  biquad1.setHighShelf( 0, freq1, tGain1, slope1 );                             //stage, freq, gain, slope
+  biquad1.setHighShelf( 0, freq2, tGain2, slope2 );                             //stage, freq, gain, slope
+  biquad1.setHighShelf( 0, freq3, tGain3, slope3 );                             //stage, freq, gain, slope
 
   // Configure Filter
   filter1.frequency(cornerFreq);                                                // Set Corner Frequency to the default value of 1500.00 Hz or 1.5 kHz
