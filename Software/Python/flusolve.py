3e9400bfd3d6195e190d917f68bab3d67675c5a1
Fluvio Lobo <fluvio.lobo@gmail.com>
2017-08-21 19:33:45 -0400

    Created a script to test the solvers used in our process

--- Software/Python/flusolve.py
+++ Software/Python/flusolve.py
@@ -12,15 +12,28 @@ from    scipy.optimize          import  broyden1
 from    scipy.optimize          import  newton_krylov
 from    scipy.optimize          import  root
 from    scipy.linalg            import  norm
-#from    sympy                   import  nsolve
-#from    usbProtocol             import  createUSBPort
-#from    sympy.core.symbol       import symbols
-#from    sympy.solvers.solveset   import nonlinsolve
 
-# Construct equations to solve for:
-def LHS( root ):
+# Functions
+def forward_lhs( const, vals ):
+    K = const
+    x, y, z = vals
+    
+    sol1 = (( K*( x**2. + y**2. + z**2. )**(-3.) *
+            ( 3.*( z**2./(x**2. + y**2. + z**2.) ) + 1 )))
+    
+    sol2 = (( K*( (x+.05)**2. + (y-.05)**2. + z**2. )**(-3.) *
+            ( 3.*( z**2./((x+.05)**2. + (y-.05)**2. + z**2.) ) + 1 )))
+    
+    sol3 = (( K*( (x-.05)**2. + (y-.05)**2. + z**2. )**(-3.) *
+            ( 3.*( z**2./((x-.05)**2. + (y-.05)**2. + z**2.) ) + 1 )))
+
+    print sol1, sol2, sol3
+    return sol1, sol2, sol3
+    
+
+def backward_lhs( root ):
+    K = const
     x, y, z = root
-    H1_L2, H2_L2, H3_L2 = HNorm
     sensor1 = ( K*( x**2. + y**2. + z**2. )**(-3.) *
               ( 3.*( z**2./(x**2. + y**2. + z**2.) ) + 1 ))
     
@@ -30,18 +43,25 @@ def LHS( root ):
     sensor3 = ( K*( (x-.05)**2. + (y-.05)**2. + z**2. )**(-3.) *
               ( 3.*( z**2./((x-.05)**2. + (y-.05)**2. + z**2.) ) + 1 ))
     
-    return ( sensor1 - H1_L2**2, sensor2 - H2_L2**2, sensor3 - H3_L2**2 )
+    return ( sensor1**2 - sol1, sensor2**2 - sol2, sensor3**2 - sol3)
 
 
-# Define constants
+
+# Evaluate normal function with an arbitrary value
+const = 1.20                                                # random value of K
+vals = [1.0,2.0,0.5]                                        # random positions
+sol1, sol2, sol3 = forward_lhs(const, vals)                 # forward solution
 
 
-# Solve function
-sol = root(LHS1, initialGuess, method='lm', options={'maxiter':250000})
+# Solve function using approximate intitial guess
+guess = [1.0,2.0,0.50]
+
+sol = root(backward_lhs, guess, method='lm', options={'maxiter':250000})
 print( sol ) 
 print( "SOLUTION:" )
 print( "x = %.5f || y = %.5f || z = %.5f" %(sol.x[0], sol.x[1], sol.x[2]) )
 solution = np.array((sol.x[0], sol.x[1], sol.x[2]), dtype='f')
-print( LHS1(solution) )
+print( backward_lhs(solution) )
 sleep( .5 )
+
 
