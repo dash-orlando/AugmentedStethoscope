70cff6ec410f5fe1eedc0897cff9c5fe62258b38
moe93 <m.odeh93@gmail.com>
2017-09-06 10:47:15 -0400

    Error checks on sensors

--- Software/Arduino/IMU_v3/IMU_v3.ino
+++ Software/Arduino/IMU_v3/IMU_v3.ino
@@ -8,7 +8,10 @@
  * DATE       : Jun. 27th, 2017, Year of Our Lord
  * 
  * MODIFIED BY: Mohammad Odeh
- * DATE       : Aug. 18th, 2017, Year of Our Lord
+ * DATE       : Sep. 06th, 2017, Year of Our Lord
+ * 
+ * CHANGELOG:-
+ *  1- Error check to verify if sensor is connected/working or not
  */
 
 // Include required libraries
@@ -19,21 +22,22 @@
 // Define important constants
 #define LSM9DS1_M             0x1E    // Would be 0x1C if SDO_M is LOW
 #define LSM9DS1_AG            0x6B    // Would be 0x6A if SDO_AG is LOW
-#define PRINT_DELAY           10      // 40Hz data readings @ 20 [Milliseconds]
 #define CALIBRATION_INDEX     50      // Accounting for ambient magnetic fields
-#define DECLINATION           6.55    // Accounting for the Earth's magnetic field
 #define BAUDRATE              115200  // Serial communication baudrate
 #define NPINS                 3       // Number of select pins
-#define NSENS                 3       // Number of select pins
+#define NSENS                 3       // Number of sensors
 
-
-LSM9DS1 imu;                    // Declare sensor class
+LSM9DS1 imu;                    // Instantiate sensors
 byte Sx_pin[3]  = {13, 12, 11}; // Select pins: {S0, S1, S2}
 
 // Calibration (BASE) readings
 static double imu_BASE[3][3] =  { {0, 0, 0},    //  {1x, 1y, 1z}
                                   {0, 0, 0},    //  {2x, 2y, 2z}
                                   {0, 0, 0} };  //  {3x, 3y, 3z}
+
+// Enumerate sensor states ( 0==sensor1, 1==sensor2, ..., n==sensor(n+1) )
+enum State { SEN_OK, SEN_ERR };
+State sensorState[3]  = { SEN_ERR, SEN_ERR, SEN_ERR };  // Is the sensor detected and working?
 
 void setup() {
   Serial.begin( BAUDRATE );             // Start serial monitor
@@ -61,35 +65,36 @@ void loop() {
   static double imu_RAW = 0;
 
   // Obtain readings from ALL sensors
-  for (byte i = 0; i < NSENS; i++) {    // Loop over ROWS
-    for (byte j = 0; j < NSENS; j++) {  // Loop over COLUMNS
-      setSensor(i);                     // Loop over sensors
-      //delay( PRINT_DELAY / 2 );         // Print speed
+  for (byte i = 0; i < NSENS; i++) {      // Loop over ROWS     (Sensors)
+    for (byte j = 0; j < NSENS; j++) {    // Loop over COLUMNS  (Axes)
+      
+      if (sensorState[i] == SEN_OK) {     // Set Sensor IFF it was INITIALIZED
+        setSensor(i);                     // Loop over sensors
+        while ( !imu.magAvailable() );    // Check if sensor is ready
+  
+        imu.readMag();                    // Do readings
+        if (j == 0){
+          imu_RAW = double( imu.calcMag(imu.mx) );
+        }
+        else if ( j == 1) {
+          imu_RAW = double( imu.calcMag(imu.my) );
+        }
+        else if ( j == 2) {
+          imu_RAW = double( imu.calcMag(imu.mz) );
+        }
+  
+        // Subtract BASE readings from RAW readings to get CALIBRATED readings
+        B[i][j] =  imu_RAW - imu_BASE[i][j];
+      } 
 
-      while ( !imu.magAvailable() );    // Check if sensor is ready
-
-      imu.readMag();                    // Do readings
-      if (j == 0){
-        imu_RAW = double( imu.calcMag(imu.mx) );
-      }
-      else if ( j == 1) {
-        imu_RAW = double( imu.calcMag(imu.my) );
-      }
-      else if ( j == 2) {
-        imu_RAW = double( imu.calcMag(imu.mz) );
-      }
-
-      // Subtract BASE readings from RAW readings to get CALIBRATED readings
-      B[i][j] =  imu_RAW - imu_BASE[i][j];
-
-      // Print readings to Serial
-      Serial.print(B[i][j], 5);
+      // Make output look pretty
+      Serial.print(B[i][j], 5);           // Print readings to Serial
       if ( (i == NSENS - 1) && (j == NSENS - 1) ) {
-        Serial.print("\n");
+        Serial.print("\n");               // Print new line after last sensor and last axis
+      } else {
+        Serial.print(", ");               // Print a comma between sensor axes
       }
-      else {
-        Serial.print(", ");
-      }
+      
     }
   }
 }
@@ -121,14 +126,19 @@ void calibrateIMU(byte i) {
   // Temporary value holders needed for calibration
   double imu_hold[3] = {0, 0, 0};         // {x, y, z}
 
-  if ( !imu.begin() ) {
+  if ( !imu.begin() ) {       // Check if device is initialized
+    sensorState[i] = SEN_ERR; // Sensor ERROR
+    
     Serial.print( F("Failed to communicate with LSM9DS1 ") );
     Serial.println( i + 1 );
-    while (1);
-  } else {
-    Serial.println( F("Calibrating. Please Wait.") );
-    imu.calibrateMag();
 
+  } else {
+    sensorState[i] = SEN_OK;  // Sensor is OK
+    
+    Serial.println( F("Calibrating. Please Wait.") );
+    imu.calibrateMag();       // Call built-in function to calculate bias
+
+    // Perform our own calibration process to further clear noise
     for (int j = 0; j < CALIBRATION_INDEX ; j++) {
       delay( 50 );                        // Delay for stability
       while ( !imu.magAvailable() );      // Wait until readings are available
