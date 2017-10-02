c5484ab09c92d8f577649e7a69a0d09190711d29
moe93 <m.odeh93@gmail.com>
2017-10-02 14:12:17 -0400

    Fix errors and improve detection range

--- Software/Arduino/IMU_v3/IMU_v3.ino
+++ Software/Arduino/IMU_v3/IMU_v3.ino
@@ -1,20 +1,20 @@
 /*
- * Obtain magnetic field vector using multiplexed LSM9DS1 IMU sensors.
- * Obtained values take into account the magnetic field of the earth
- * and the immediate surrounding area and SUBTRACTS them from the readings
- * to give a calibrated reading independent of the surrounding magnetic field.
- * 
- * AUTHOR     : Edward Daniel Nichols
- * DATE       : Jun. 27th, 2017, Year of Our Lord
- * 
- * MODIFIED BY: Mohammad Odeh
- * DATE       : Sep. 27th, 2017, Year of Our Lord
- * 
- * CHANGELOG:-
- *  1- Incorporated 4th sensor
- *  2- Constrained code to defined values (to add an nth sensor only
- *     modify #define NESENS and nothing else)
- */
+   Obtain magnetic field vector using multiplexed LSM9DS1 IMU sensors.
+   Obtained values take into account the magnetic field of the earth
+   and the immediate surrounding area and SUBTRACTS them from the readings
+   to give a calibrated reading independent of the surrounding magnetic field.
+
+   AUTHOR     : Edward Daniel Nichols
+   DATE       : Sept. 29th, 2017, Year of Our Lord
+
+   AUTHOR     : Mohammad Odeh
+   DATE       : Sept. 29th, 2017, Year of Our Lord
+
+   CHANGELOG:-
+    1- Incorporated 4th sensor
+    2- Constrained code to defined values (to add an nth sensor only
+       modify #define NSENS and nothing else)
+*/
 
 // Include required libraries
 #include <Wire.h>
@@ -27,47 +27,54 @@
 #define CALIBRATION_INDEX     50      // Accounting for ambient magnetic fields
 #define BAUDRATE              115200  // Serial communication baudrate
 #define NPINS                 3       // Number of select pins
-#define NSENS                 4       // Number of sensors
+#define NSENS                 6       // Number of sensors
 #define NAXES                 3       // Number of axes
+#define LED                   1       // "All iz Gud" Indicator LED
 
 LSM9DS1 imu;                          // Instantiate sensors
-byte Sx_pin[3]  = {10, 9, 8};         // Select pins: {S0, S1, S2}
+byte Sx_pin[3]  = {8, 9, 10};         // Select pins: {S0, S1, S2}
 
 // Calibration (BASE) readings
 static double imu_BASE[NSENS][NAXES] =  { {0, 0, 0},    //  {1x, 1y, 1z}
                                           {0, 0, 0},    //  {2x, 2y, 2z}
                                           {0, 0, 0},    //  {3x, 3y, 3z}
-                                          {0, 0, 0} };  //  {4x, 4y, 4z}
+                                          {0, 0, 0},    //  {4x, 4y, 4z}
+                                          {0, 0, 0},    //  {5x, 5y, 5z}
+                                          {0, 0, 0} };  //  {6x, 6y, 6z}
 
 // Enumerate sensor states ( 0==sensor1, 1==sensor2, ..., n==sensor(n+1) )
 enum State { SEN_ERR, SEN_OK };
 State sensorState[NSENS]  = { };      // Is the sensor detected and working?
 
 void setup() {
-  Serial.begin( BAUDRATE );             // Start serial monitor
-  
-  pinMode(13, OUTPUT);                  // Turn LED ON to know
-  digitalWrite(13, HIGH);               // Teensy ain't dead
+  Serial.begin( BAUDRATE );           // Start serial monitor
+
+  pinMode( 13, OUTPUT );              // Turn LED ON to know
+  digitalWrite( 13, HIGH );           // Teensy ain't dead
+  pinMode( LED, OUTPUT );             // Set LED pin to OUTPUT
 
   for ( byte i = 0; i < NPINS; i++ ) {
-    pinMode( Sx_pin[i], OUTPUT );       // Set "Select Pins" as output
+    pinMode( Sx_pin[i], OUTPUT );     // Set "Select Pins" as output
   }
 
   // Initialize sensors and load settings
   for (byte i = 0; i < NSENS; i++) {
-    setSensor(i);         // Switch between sensors
-    setupIMU();           // Setup IMUs
-    calibrateIMU(i);      // Calibrate Sensors
-  } Serial.println( F("Success.") );
+    setSensor(i);                     // Switch between sensors
+    setupIMU();                       // Setup IMUs
+    calibrateIMU(i);                  // Calibrate Sensors
+  } Serial.println( F("Success.") );  // SUCCESS!
+    digitalWrite( LED, HIGH );        // Turn LED ON!
 }
 
 void loop() {
 
   // IMU readings (CALIBRATED) matrix
-  static double B[NSENS][NAXES]={ {0, 0, 0},      //  {B1_x, B1_y, B1_z}
-                                  {0, 0, 0},      //  {B2_x, B2_y, B2_z}
-                                  {0, 0, 0},      //  {B3_x, B3_y, B3_z}
-                                  {0, 0, 0} };    //  {B4_x, B4_y, B4_z}
+  static double B[NSENS][NAXES] = { {0, 0, 0},    //  {B1_x, B1_y, B1_z}
+                                    {0, 0, 0},    //  {B2_x, B2_y, B2_z}
+                                    {0, 0, 0},    //  {B3_x, B3_y, B3_z}
+                                    {0, 0, 0},    //  {B4_x, B4_y, B4_z}
+                                    {0, 0, 0},    //  {B5_x, B5_y, B5_z}
+                                    {0, 0, 0} };  //  {B6_x, B6_y, B6_z}
 
   // IMU readings (RAW)
   static double imu_RAW = 0;
@@ -75,13 +82,13 @@ void loop() {
   // Obtain readings from ALL sensors
   for (byte i = 0; i < NSENS; i++) {      // Loop over ROWS     (Sensors)
     for (byte j = 0; j < NAXES; j++) {    // Loop over COLUMNS  (Axes)
-      
+
       if (sensorState[i] == SEN_OK) {     // Set Sensor IFF it was INITIALIZED
         setSensor(i);                     // Loop over sensors
         while ( !imu.magAvailable() );    // Check if sensor is ready
-  
+
         imu.readMag();                    // Do readings
-        if (j == 0){
+        if (j == 0) {
           imu_RAW = double( imu.calcMag(imu.mx) );
         }
         else if ( j == 1) {
@@ -90,10 +97,10 @@ void loop() {
         else if ( j == 2) {
           imu_RAW = double( imu.calcMag(imu.mz) );
         }
-  
+
         // Subtract BASE readings from RAW readings to get CALIBRATED readings
         B[i][j] =  imu_RAW - imu_BASE[i][j];
-      } 
+      }
 
       // Make output look pretty
       Serial.print(B[i][j], 5);           // Print readings to Serial
@@ -102,7 +109,6 @@ void loop() {
       } else {
         Serial.print(", ");               // Print a comma between sensor axes
       }
-      
     }
   }
 }
@@ -136,13 +142,13 @@ void calibrateIMU(byte i) {
 
   if ( !imu.begin() ) {                   // Check if device is initialized
     sensorState[i] = SEN_ERR;             // Sensor ERROR
-    
+
     Serial.print( F("Failed to communicate with LSM9DS1 ") );
     Serial.println( i + 1 );
 
   } else {
     sensorState[i] = SEN_OK;              // Sensor is OK
-    
+
     Serial.println( F("Calibrating. Please Wait.") );
     imu.calibrateMag();                   // Call built-in function to calculate bias
 
@@ -169,14 +175,14 @@ void calibrateIMU(byte i) {
 // =========================    Set Sensor      ========================
 // Set the value on the multiplexer to set the sensor to use
 void setSensor( byte sensorIndex ) {
-  // Sensor 1
+  // Sensor 1, 000
   if (sensorIndex == 0 ) {
     for (byte i = 0; i < NPINS; i++) {
       digitalWrite(Sx_pin[i], LOW);
     }
   }
 
-  // Sensor 2
+  // Sensor 2, 001
   else if (sensorIndex == 1 ) {
     for (byte i = 0; i < NPINS; i++) {
       if (i == 0) {
@@ -185,8 +191,7 @@ void setSensor( byte sensorIndex ) {
     }
   }
 
-
-  // Sensor 3
+  // Sensor 3, 010
   else if (sensorIndex == 2) {
     for (byte i = 0; i < NPINS; i++) {
       if (i == 1) {
@@ -195,10 +200,28 @@ void setSensor( byte sensorIndex ) {
     }
   }
 
-  // Sensor 4
+  // Sensor 4, 011
   else if (sensorIndex == 3) {
     for (byte i = 0; i < NPINS; i++) {
       if (i == 2) {
+        digitalWrite(Sx_pin[i], LOW);
+      } else digitalWrite(Sx_pin[i], HIGH);
+    }
+  }
+
+  // Sensor 5, 100
+  else if (sensorIndex == 4) {
+    for (byte i = 0; i < NPINS; i++) {
+      if (i == 2) {
+        digitalWrite(Sx_pin[i], HIGH);
+      } else digitalWrite(Sx_pin[i], LOW);
+    }
+  }
+
+  // Sensor 6, 101
+  else if (sensorIndex == 5) {
+    for (byte i = 0; i < NPINS; i++) {
+      if (i == 1) {
         digitalWrite(Sx_pin[i], LOW);
       } else digitalWrite(Sx_pin[i], HIGH);
     }
--- Software/Python/Finexus_Method.py
+++ Software/Python/Finexus_Method.py
@@ -6,24 +6,26 @@
 * Position tracking of magnet based on Finexus
 * https://ubicomplab.cs.washington.edu/pdfs/finexus.pdf
 *
-* VERSION: 0.1.1
+* VERSION: 0.1.6
 *   - First working version.
 *   - FIXED: Resolved buffer issue by switching from Arduino to Teensy
 *     for the data acquisition and communications.
 *   - FIXED: Added a check to verify that solutions (distances) make
 *     physical sense. If check fails, the code readjusts calculations.
+*   - MODIFIED: Grid is now based off a circular array
+*   - ADDED: Use 6 sensors instead of 4.
 *
 * KNOWN ISSUES:
-*   - Small grid size
-*     (Look into a better approach for seperating the incoming data)
 *   - Calculations are accurate to around +/-3mm
 *     (Look into improving K estimate)
+*   - Solutions do NOT make any sense! :c
 *
-* AUTHOR  :   Daniel Nichols, Mohammad Odeh
-* DATE    :   Aug. 04th, 2017 Year of Our Lord
+*
+* AUTHOR  :   Edward Nichols
+* DATE    :   Sept. 29th, 2017 Year of Our Lord
 * 
-* MODIFIED:   Mohammad Odeh
-* DATE    :   Sep. 25th, 2017 Year of Our Lord
+* AUTHOR  :   Mohammad Odeh 
+* DATE    :   Sept. 29th, 2017 Year of Our Lord
 *
 '''
 
@@ -36,23 +38,34 @@ from    usbProtocol         import  createUSBPort   # Create USB port (serial co
 import  argparse                                    # Feed in arguments to the program
 
 # ************************************************************************
-# =====================> CONSTRUCT ARGUMENT PARSER <=====================
+# =====================> CONSTRUCT ARGUMENT PARSER <=====================*
 # ************************************************************************
 ap = argparse.ArgumentParser()
 
-ap.add_argument("-d", "--debug", action='store_true',
+ap.add_argument("-hd", "--hardDebug", action='store_true',
+                help="invoke flag to enable debugging")
+ap.add_argument("-sd", "--softDebug", action='store_true',
                 help="invoke flag to enable debugging")
 
 args = vars( ap.parse_args() )
 
-#args["debug"] = False
+##args["debug"] = True
+args["softDebug"] = True
 
 # ************************************************************************
-# =====================> DEFINE NECESSARY FUNCTIONS <=====================
+# =====================> DEFINE NECESSARY FUNCTIONS <====================*
 # ************************************************************************
 
 # ****************************************************
-# Define function to pool & return data from Arduino
+# Define function to sort from lowest->highest value *
+# ****************************************************
+def argsort(seq):
+    # http://stackoverflow.com/questions/3071415/efficient-method-to-calculate-the-rank-vector-of-a-list-in-python
+    return sorted(range(len(seq)), key=seq.__getitem__)
+
+
+# ****************************************************
+# Define function to pool & return data from Arduino *
 # ****************************************************
 def getData(ser):
     global CALIBRATING
@@ -66,13 +79,13 @@ def getData(ser):
 
     try:
         # Read incoming data and seperate
-        line = ser.readline()[:-1]
-        col = line.split(",")
+        line    = ser.readline()[:-1]
+        col     = line.split(",")
 
         # Wait for the sensor to calibrate itself to ambient fields.
-        while( len(col) < 9 ):
-            line = ser.readline()[:-1]
-            col = line.split(",")
+        while( len(col) < 18 ):
+            line    = ser.readline()[:-1]
+            col     = line.split(",")
             if(CALIBRATING == True):
                 print( "Calibrating...\n" )
                 CALIBRATING = False
@@ -97,8 +110,26 @@ def getData(ser):
             Bz=float(col[8])
             B3 = np.array( ([Bx],[By],[Bz]), dtype='float64') # Units { G }
 
+            # Sensor 4
+            Bx=float(col[9] )
+            By=float(col[10])
+            Bz=float(col[11])
+            B4 = np.array( ([Bx],[By],[Bz]), dtype='float64') # Units { G }
+
+            # Sensor 5
+            Bx=float(col[12] )
+            By=float(col[13])
+            Bz=float(col[14])
+            B5 = np.array( ([Bx],[By],[Bz]), dtype='float64') # Units { G }
+
+            # Sensor 6
+            Bx=float(col[15] )
+            By=float(col[16])
+            Bz=float(col[17])
+            B6 = np.array( ([Bx],[By],[Bz]), dtype='float64') # Units { G }
+            
             # Return vectors
-            return (B1, B2, B3)
+            return (B1, B2, B3, B4, B5, B6)
 
     except Exception as e:
         print( "Caught error in getData()"      )
@@ -109,23 +140,59 @@ def getData(ser):
 # Define function to construct equations to solve for
 # ****************************************************
 def LHS( root, K, norms ):
+    global PRINT
+    global sort
+    
     # Extract x, y, and z
     x, y, z = root
     
     # Construct the (r) terms for each sensor
-    # NOTE: relative distance terms are in meters
-    r1 = float( ( (x+0.00)**2. + (y+0.00)**2. + (z+0.00)**2. )**(1/2.) )    # Sensor 1
-    r2 = float( ( (x+0.10)**2. + (y-0.10)**2. + (z+0.00)**2. )**(1/2.) )    # Sensor 2
-    r3 = float( ( (x-0.10)**2. + (y-0.10)**2. + (z+0.00)**2. )**(1/2.) )    # Sensor 3
+    # NOTE: Relative distance terms are in meters
+    #     : Standing on sensor(n), how many units in
+    #       the x/y/z direction should I march to get
+    #       back to sensor1 (origin)?
+
+    # NOTE TO MOE 9/29 6pm: The sensor array is rejigged to a circle. Motion doesn't follow the coordinate axis written on the paper:
+    # i.e. If the magnet was moved 25mm in positive X, the Y and Z axes were the ones that shifted.
+    # I was in the process of verifying the definitions to match the axis definitions on the IMUs and our physical motion.
+    # Needless to say, the magnitude of motion is not accurate either.
+
+    r1 = float( ( (x+0.000)**2. + (y-0.125)**2. + (z+0.00)**2. )**(1/2.) )    # Sensor 1
+    r2 = float( ( (x-0.100)**2. + (y-0.175)**2. + (z+0.00)**2. )**(1/2.) )    # Sensor 2
+    r3 = float( ( (x-0.200)**2. + (y-0.125)**2. + (z+0.00)**2. )**(1/2.) )    # Sensor 3
+    r4 = float( ( (x+0.000)**2. + (y+0.000)**2. + (z+0.00)**2. )**(1/2.) )    # Sensor 4
+    r5 = float( ( (x-0.100)**2. + (y+0.050)**2. + (z+0.00)**2. )**(1/2.) )    # Sensor 5
+    r6 = float( ( (x-0.200)**2. + (y-0.000)**2. + (z+0.00)**2. )**(1/2.) )    # Sensor 6
 
     # Construct the equations
     Eqn1 = ( K*( r1 )**(-6.) * ( 3.*( z/r1 )**2. + 1 ) ) - norms[0]**2.     # Sensor 1
     Eqn2 = ( K*( r2 )**(-6.) * ( 3.*( z/r2 )**2. + 1 ) ) - norms[1]**2.     # Sensor 2
     Eqn3 = ( K*( r3 )**(-6.) * ( 3.*( z/r3 )**2. + 1 ) ) - norms[2]**2.     # Sensor 3
+    Eqn4 = ( K*( r4 )**(-6.) * ( 3.*( z/r4 )**2. + 1 ) ) - norms[3]**2.     # Sensor 4
+    Eqn5 = ( K*( r5 )**(-6.) * ( 3.*( z/r5 )**2. + 1 ) ) - norms[4]**2.     # Sensor 5
+    Eqn6 = ( K*( r6 )**(-6.) * ( 3.*( z/r6 )**2. + 1 ) ) - norms[5]**2.     # Sensor 6
 
     # Construct a vector of the equations
-    f = [Eqn1, Eqn2, Eqn3]
+    Eqns = [Eqn1, Eqn2, Eqn3, Eqn4, Eqn5, Eqn6]
 
+    # Determine which sensors to use based on magnetic field value (smallValue==noBueno!)
+    sort = argsort(norms)
+    sort.reverse()
+    f=[]
+    for i in range(0, 3):
+        f.append(Eqns[sort[i]])
+
+    # If in debug/verbose mode:
+    if (args["hardDebug"]):
+        if (PRINT):
+            print( "\n=========================================" )
+            print( "Norms: %f, %f, %f, %f, %f, %f" %(norms[0],norms[1],norms[2],norms[3],norms[4],norms[5]) )
+            print( "Length of (F) vector is: %d" %len(f))
+            print( "Using sensors: #%d, #%d, #%d" %(sort[0]+1, sort[1]+1, sort[2]+1) )
+            print( "=========================================\n" )
+            sleep(1.5)
+            PRINT = False
+        
     # Return vector
     return ( f )
 
@@ -135,18 +202,28 @@ def LHS( root, K, norms ):
 
 # Useful variables
 global CALIBRATING
+global PRINT
+global sort
+sort = 'null'
 
 CALIBRATING = True                              # Boolean to indicate that device is calibrating
-READY       = False                             # Give time for user to palce magnet
+READY       = False                             # Give time for user to place magnet
+PRINT       = True                              # A flag to print ONLY once!
 
 K           = 1.09e-6                           # Magnet's constant (K) || Units { G^2.m^6}
-dx          = 1e-5                              # Differential step size (Needed for solver)
-initialGuess= np.array((.01, .075, -.01), 
+dx          = 1e-7                              # Differential step size (Needed for solver)
+
+##initialGuess= np.array((.01, .075, -.01), 
+##                        dtype='float64' )     # Initial position/guess
+initialGuess= np.array((.100, 0.000, 0.000), 
                         dtype='float64' )       # Initial position/guess
 
 # Establish connection with Arduino
+DEVC = "Arduino"
+PORT = 29
+BAUD = 115200
 try:
-    IMU = createUSBPort( "Arduino", 31, 115200 )
+    IMU = createUSBPort( DEVC, PORT, BAUD )
     if IMU.is_open == False:
         IMU.open()
     print( "Serial Port OPEN" )
@@ -166,11 +243,11 @@ except Exception as e:
 # Start iteration
 while( True ):
     # Pool data from Arduino
-    (H1, H2, H3) = getData(IMU)
+    (H1, H2, H3, H4, H5, H6) = getData(IMU)
 
     # Inform user that system is almost ready
     if(READY == False):
-        print( "Place magnet @ (x, y, z) = (0, 75, 0)mm\n" )
+        print( "Place magnet @ the starting location! \n" )
         sleep( 2.5 )
         print( "Ready in 5" )
         sleep( 1.0 )
@@ -188,7 +265,7 @@ while( True ):
         READY = True
         
     # Compute L2 vector norms
-    HNorm = [float(norm(H1)), float(norm(H2)), float(norm(H3))]
+    HNorm = [float(norm(H1)), float(norm(H2)), float(norm(H3)), float(norm(H4)), float(norm(H5)), float(norm(H6))]
 
     # Invoke solver
     sol = root(LHS, initialGuess, args=(K, HNorm), method='lm',
@@ -197,10 +274,14 @@ while( True ):
 
     # Print solution (coordinates) to screen
     print( "Current position (x , y , z):" )
-    print( "(%.5f , %.5f , %.5f)mm\n" %(sol.x[0]*1000, sol.x[1]*1000, sol.x[2]*1000) )
-
+    print( "(%.5f , %.5f , %.5f)mm" %(sol.x[0]*1000, sol.x[1]*1000, sol.x[2]*1000) )
+    if (args["softDebug"]):
+        print( "Using sensors: #%d, #%d, #%d\n\n" %(sort[0]+1, sort[1]+1, sort[2]+1) )
+    print('')
+    
     # If in debug/verbose mode:
-    if (args["debug"]):
+    if (args["hardDebug"]):
+        PRINT = True
         # Print complete solution returned by vector
         print( sol )
         print("")
@@ -211,7 +292,7 @@ while( True ):
     # Check if solution makes sense
     if (abs(sol.x[0]*1000) > 500) or (abs(sol.x[1]*1000) > 500) or (abs(sol.x[2]*1000) > 500):
         print( "Invalid solution. Resetting Calculations" )
-        initialGuess = np.array( (.01, .075, -.01), dtype='float64' )
+        initialGuess = np.array( (-0.100, -0.025, 0.005), dtype='float64' )
         
     # Update initial guess with current position and feed back to solver
     else:    
