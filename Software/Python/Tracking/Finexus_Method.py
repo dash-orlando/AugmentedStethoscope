51955deab2ab87b1d54c2e928d90b0d1800d5f98
Moe Odeh <m.odeh93@gmail.com>
2017-11-08 11:38:20 -0500

    Beautify code + bug fixes
    - Fixed major crashing issue due to the incoming array (from MCU) not being properly filled and passed on
    - Streamlined code to make it easier to read and more visually appealing (useful while debugging)

--- Software/Python/Tracking/Finexus_Method.py
+++ Software/Python/Tracking/Finexus_Method.py
@@ -3,29 +3,29 @@
 * Position tracking of magnet based on Finexus
 * https://ubicomplab.cs.washington.edu/pdfs/finexus.pdf
 *
-* VERSION: 0.2.5
-*   - MODIFIED: Removed limitation of needing to place magnet at a
-*               predefined position at program start up.
-*   - ADDED   : 6 sensors placed around an ellipse
+* VERSION: 0.3.0
+*   - FIXED   : Program now does a check on the received data
+*               to avoid the error we get so often regarding
+*               the array containing invalid data
+*   - MODIFIED: Streamlined code to make it more human friendly
+*   - MODIFIED: Modified code to go along with the 2 I2C addresses
+*               fix provided by Danny
 *   - ADDED   : Finding the initial guess on program startup is now
 *               dynamic; it is calculated on the spot rather than
 *               choosing from a hardcoded list of all the possible
 *               initial guesses.
-*   - MODIFIED: Modified code to go along with the 2 I2C addresses
-*               fix provided by Danny
 *
 * KNOWN ISSUES:
-*   - Calculations are accurate to around +/-2mm
-*     (this is very beuno, if not perfect)
+*   - Loss in accuracy in 3D space  (not even surprised)
+*   - Slow output rates             (look into multithreading)
 *   - At certain positions, the z position goes cray-cray
-*     while x & y are correct.
-*     (Look into what triggers this behaviour)
+*     while x & y are correct.      (Look into what triggers this behaviour)
 *
 * AUTHOR                    :   Edward Nichols
 * LAST CONTRIBUTION DATE    :   Sep. 29th, 2017 Year of Our Lord
 * 
 * AUTHOR                    :   Mohammad Odeh 
-* LAST CONTRIBUTION DATE    :   Oct. 26th, 2017 Year of Our Lord
+* LAST CONTRIBUTION DATE    :   Nov. 08th, 2017 Year of Our Lord
 *
 '''
 
@@ -56,29 +56,36 @@ args["visualize-position"] = True
 # =====================> DEFINE NECESSARY FUNCTIONS <====================*
 # ************************************************************************
 
-# ****************************************************
-# Define function to sort from lowest->highest value *
-# -------------------------------------------------- *
-# INPUT :   - A list                                 *
-# OUTPUT:   - A list containing the indices of the   *
-#             given list's elements arranged from    *
-#             the index of the element with the      *
-#             smallest value to the index of the     *
-#             element with the largest value         *
-# ****************************************************
-def argsort(seq):
+def argsort( seq ):
+    '''
+    Sort a list's elements from smallest to largest and
+    return the sorted INDICES NOT VALUES!
+    
+    INPUTS:
+        - seq: A list whose elements are to be sorted 
+
+    OUTPUT:
+        - A list containing the indices of the given list's elements
+          arranged from the index of the element with the smallest
+          value to the index of the element with the largest value
+    '''
     # http://stackoverflow.com/questions/3071415/efficient-method-to-calculate-the-rank-vector-of-a-list-in-python
     return sorted(range(len(seq)), key=seq.__getitem__)
 
-# ****************************************************
-# Sort a list's elements from smallest to largest    *
-# -------------------------------------------------- *
-# INPUT :   - List to be sorted                      *
-#           - Number of elements in said list that   *
-#               you want to sort                     *
-# OUTPUT:   - A sorted list of size (N)              *
-# ****************************************************
-def bubbleSort(arr, N):
+# --------------------------
+
+def bubbleSort( arr, N ):
+    '''
+    Sort a list's elements from smallest to largest 
+    
+    INPUTS:
+        - arr: List to be sorted
+        - N  : Number of elements in said list that need to be sorted
+                (i.e list has 5 elements, if N=3, sort the first 3)
+
+    OUTPUT:
+        - A sorted list of size N
+    '''
     data = []
     for i in range(0, N):
         data.append( arr[i] )
@@ -93,10 +100,24 @@ def bubbleSort(arr, N):
                 continue
     return (data)
 
-# ****************************************************
-# Define function to pool & return data from Arduino *
-# ****************************************************
-def getData(ser):
+# --------------------------
+
+def getData( ser ):
+    '''
+    Pool the data from the MCU (wheteher it be a Teensy or an Arduino or whatever)
+    The data consists of the magnetic field components in the x-, y-, and z-direction
+    of all the sensors. The array must begin with '<' as the SOH signal, the compononents
+    must be comma delimited, and must end with '>' as the EOT signal.
+    
+            >$\     <B_{1x}, B_{1y}, B_{1z}, ..., B_{1x}, B_{1y}, B_{1z}> 
+    
+    INPUTS:
+        - ser: a serial object. Note that the serial port MUST be open before
+               passing it the to function
+
+    OUTPUT:
+        - Individual numpy arrays of all the magnetic field vectors
+    '''
     global CALIBRATING
 
     # Flush buffer
@@ -104,7 +125,7 @@ def getData(ser):
     ser.reset_output_buffer()
 
     # Allow data to fill-in buffer
-    #sleep(0.1)
+    # sleep(0.1)
 
     try:
         # Wait for the sensor to calibrate itself to ambient fields.
@@ -127,61 +148,78 @@ def getData(ser):
                 line = line + inData
 
         # Split line into the constituent components
-        col     = line.split(",")
+        col     = (line.rstrip()).split(",")
+        if (len(col) == 18):
+            #
+            # Construct magnetic field array
+            #
 
-        #
-        # Construct magnetic field array
-        #
+            # Sensor 1
+            Bx = float( col[0] )
+            By = float( col[1] )
+            Bz = float( col[2] )
+            B1 = np.array( ([Bx],[By],[Bz]), dtype='float64') # Units { G }
 
-        # Sensor 1
-        Bx = float(col[0])
-        By = float(col[1])
-        Bz = float(col[2])
-        B1 = np.array( ([Bx],[By],[Bz]), dtype='float64') # Units { G }
+            # Sensor 2
+            Bx = float( col[3] )
+            By = float( col[4] )
+            Bz = float( col[5] )
+            B2 = np.array( ([Bx],[By],[Bz]), dtype='float64') # Units { G }
 
-        # Sensor 2
-        Bx = float(col[3])
-        By = float(col[4])
-        Bz = float(col[5])
-        B2 = np.array( ([Bx],[By],[Bz]), dtype='float64') # Units { G }
+            # Sensor 3
+            Bx = float( col[6] )
+            By = float( col[7] )
+            Bz = float( col[8] )
+            B3 = np.array( ([Bx],[By],[Bz]), dtype='float64') # Units { G }
 
-        # Sensor 3
-        Bx = float(col[6])
-        By = float(col[7])
-        Bz = float(col[8])
-        B3 = np.array( ([Bx],[By],[Bz]), dtype='float64') # Units { G }
+            # Sensor 4
+            Bx = float( col[9]  )
+            By = float( col[10] )
+            Bz = float( col[11] )
+            B4 = np.array( ([Bx],[By],[Bz]), dtype='float64') # Units { G }
 
-        # Sensor 4
-        Bx = float(col[9] )
-        By = float(col[10])
-        Bz = float(col[11])
-        B4 = np.array( ([Bx],[By],[Bz]), dtype='float64') # Units { G }
+            # Sensor 5
+            Bx = float( col[12] )
+            By = float( col[13] )
+            Bz = float( col[14] )
+            B5 = np.array( ([Bx],[By],[Bz]), dtype='float64') # Units { G }
 
-        # Sensor 5
-        Bx = float(col[12])
-        By = float(col[13])
-        Bz = float(col[14])
-        B5 = np.array( ([Bx],[By],[Bz]), dtype='float64') # Units { G }
+            # Sensor 6
+            Bx = float( col[15] )
+            By = float( col[16] )
+            Bz = float( col[17] )
+            B6 = np.array( ([Bx],[By],[Bz]), dtype='float64' )# Units { G }
+            
+            # Return vectors
+            return ( B1, B2, B3, B4, B5, B6 )
 
-        # Sensor 6
-        Bx = float(col[15])
-        By = float(col[16])
-        Bz = float(col[17])
-        B6 = np.array( ([Bx],[By],[Bz]), dtype='float64') # Units { G }
-
-        # Return vectors
-        return (B1, B2, B3, B4, B5, B6)
-    
     except Exception as e:
         print( "Caught error in getData()"      )
         print( "Error type %s" %str(type(e))    )
         print( "Error Arguments " + str(e.args) )
-        getData(ser)
 
-# ****************************************************
-# Define function to construct equations to solve for
-# ****************************************************
+# --------------------------
+
 def LHS( root, K, norms ):
+    '''
+    Construct the left hand side (LHS) of the equations
+    to numerically solve for.
+    Recall that in order to solve a system numerically it
+    must have the form of,
+    
+                >$\  f(x, y, z, ...) = LHS = 0
+    
+    INPUTS:
+        - root  : a numpy array contating the initial guesses of the roots
+        - K     : K is a property of the magnet and has units of { G^2.m^6}
+        - norms : An array/list of the vector norms of the magnetic field
+                  vectors for all the sensors
+
+    OUTPUT:
+        - An array of equations that are sorted corresponding to which
+          3 sensors' equations are going to be used with the LMA solver.
+          The sorting is based on which 3 sensors are closest to the magnet.
+    '''
     global PRINT
     
     # Extract x, y, and z
@@ -211,26 +249,38 @@ def LHS( root, K, norms ):
     Eqns = [Eqn1, Eqn2, Eqn3, Eqn4, Eqn5, Eqn6]
 
     # Determine which sensors to use based on magnetic field value (smallValue==noBueno!)
-    sort = argsort(norms)               # Auxiliary function sorts norms from smallest to largest
+    sort = argsort( norms )             # Auxiliary function sorts norms from smallest to largest
     sort.reverse()                      # Python built-in function reverses elements of list
     f=[]                                # Declare vector to hold relevant functions
 
     for i in range(0, 3):               # Fill functions' array with the equations that correspond to
-        f.append(Eqns[sort[i]])         # the sensors with the highest norm, thus closest to magnet
+        f.append( Eqns[sort[i]] )       # the sensors with the highest norm, thus closest to magnet
         
     # Return vector
     return ( f )
 
-# ****************************************************
-# Determine initial guess based on magnitude of      *
-# magnetic field relative to all the sensors         *
-# ****************************************************
-def findIG(magFields):
+# --------------------------
+
+def findIG( magFields ):
+    '''
+    Dynamic search of initial guess for the LMA solver based on magnitude
+    of the magnetic field relative to all the sensors.
+    A high magnitude reading indicates magnet is close to some 3
+    sensors, the centroid of the traingle created by said sensors
+    is fed as the initial guess
+    
+    INPUTS:
+        - magfield: a numpy array containing all the magnetic field readings
+
+    OUTPUT:
+        - A numpy array containing <x, y, z> values for the initial guess
+    '''
+    
     # Define IMU positions on the grid
     #      / sensor 1: (x, y, z)
     #     /  sensor 2: (x, y, z)
     # Mat=      :          :
-    #     \     :          : Determine initial guess based on magnet's location
+    #     \     :          :
     #      \ sensor 6: (x, y, z)
     IMU_pos = np.array(((0.0  , 0.0  ,   0.0) ,
                         (0.0  , 0.125,   0.0) ,
@@ -243,15 +293,15 @@ def findIG(magFields):
     (H1, H2, H3, H4, H5, H6) = magFields
 
     # Compute L2 vector norms
-    HNorm = [ float(norm(H1)), float(norm(H2)),
-              float(norm(H3)), float(norm(H4)),
-              float(norm(H5)), float(norm(H6)) ]
+    HNorm = [ float( norm(H1) ), float( norm(H2) ),
+              float( norm(H3) ), float( norm(H4) ),
+              float( norm(H5) ), float( norm(H6) ) ]
     
     # Determine which sensors to use based on magnetic field value (smallValue==noBueno!)
-    sort = argsort(HNorm)               # Auxiliary function sorts norms from smallest to largest
+    sort = argsort( HNorm )             # Auxiliary function sorts norms from smallest to largest
     sort.reverse()                      # Python built-in function reverses elements of list
 
-    IMUS = bubbleSort(sort, 3)
+    IMUS = bubbleSort( sort, 3 )
 
     # Return the initial guess as the centroid of the detected triangle
     return ( np.array(((IMU_pos[IMUS[0]][0]+IMU_pos[IMUS[1]][0]+IMU_pos[IMUS[2]][0])/3.,
@@ -268,9 +318,8 @@ global CALIBRATING
 CALIBRATING = True                              # Boolean to indicate that device is calibrating
 READY       = False                             # Give time for user to place magnet
 
-#K           = 1.615e-7                          # Small magnet
-K           = 1.075e-6                          # Magnet's constant (K) || Units { G^2.m^6}
-#K           = 1.09e-6                           # Magnet's constant (K) || Units { G^2.m^6}
+#K           = 1.615e-7                          # Small magnet's constant   (K) || Units { G^2.m^6}
+K           = 1.09e-6                           # Big magnet's constant     (K) || Units { G^2.m^6}
 dx          = 1e-7                              # Differential step size (Needed for solver)
 
 # Establish connection with Arduino
@@ -285,8 +334,7 @@ try:
         IMU.open()
     print( "Serial Port OPEN" )
 
-    # Determine initial guess based on magnet's location
-    initialGuess = findIG(getData(IMU))
+    initialGuess = findIG(getData(IMU))         # Determine initial guess based on magnet's location
 
 # Error handling in case serial communcation fails (2/2)
 except Exception as e:
@@ -303,8 +351,6 @@ except Exception as e:
 
 # Start iteration
 while( True ):
-    # Pool data from Arduino
-    (H1, H2, H3, H4, H5, H6) = getData(IMU)
 
     # Inform user that system is almost ready
     if(READY == False):
@@ -318,33 +364,34 @@ while( True ):
 
         # Set the device to ready!!
         READY = True
-        
-    # Compute L2 vector norms
-    HNorm = [ float(norm(H1)), float(norm(H2)),
-              float(norm(H3)), float(norm(H4)),
-              float(norm(H5)), float(norm(H6)) ]
 
-    # Invoke solver (using Levenberg-Marquardt)
-    sol = root(LHS, initialGuess, args=(K, HNorm), method='lm',
-               options={'ftol':1e-10, 'xtol':1e-10, 'maxiter':1000,
-                        'eps':1e-8, 'factor':0.001})
+    # Data acquisition
+    (H1, H2, H3, H4, H5, H6) = getData(IMU)                         # Get data from MCU
+    
+    # Compute norms
+    HNorm = [ float(norm(H1)), float(norm(H2)),                     #
+              float(norm(H3)), float(norm(H4)),                     # Compute L2 vector norms
+              float(norm(H5)), float(norm(H6)) ]                    #
+
+    # Solve system of equations
+    sol = root(LHS, initialGuess, args=(K, HNorm), method='lm',     # Invoke solver using the
+               options={'ftol':1e-10, 'xtol':1e-10, 'maxiter':1000, # Levenberg-Marquardt 
+                        'eps':1e-8, 'factor':0.001})                # Algorithm (aka LMA)
 
     # Print solution (coordinates) to screen
     print( "Current position (x , y , z):" )
     print( "(%.5f , %.5f , %.5f)mm" %(sol.x[0]*1000, sol.x[1]*1000, -1*sol.x[2]*1000) )
 
-    # Sleep for stability
-    sleep( 0.1 )
+    sleep( 0.1 )                                                    # Sleep for stability
 
     # Check if solution makes sense
     if (abs(sol.x[0]*1000) > 500) or (abs(sol.x[1]*1000) > 500) or (abs(sol.x[2]*1000) > 500):
-        print( "Invalid solution. Resetting Calculations" )
-        # Determine initial guess based on magnet's location
-        initialGuess = findIG(getData(IMU))
-        
+        initialGuess = findIG( getData(IMU) )                       # Determine initial guess based on magnet's location
+
     # Update initial guess with current position and feed back to solver
     else:    
-        initialGuess = np.array( (sol.x[0]+dx, sol.x[1]+dx, sol.x[2]+dx), dtype='float64' )
+        initialGuess = np.array( (sol.x[0]+dx, sol.x[1]+dx,         # Update the initial guess as the
+                                  sol.x[2]+dx), dtype='float64' )   # current position and feed back to LMA
 
 # ************************************************************************
 # =============================> DEPRECATED <=============================
