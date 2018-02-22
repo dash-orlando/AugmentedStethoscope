a934b1265e84d5e60fbfd84d65057da142c8d29c
moe93 <m.odeh93@gmail.com>
2018-02-22 14:17:11 -0500

    Workaround for TFT screen not working without monitor

---
+++ Software/launch_script.sh
@@ -0,0 +1,13 @@
+cd /home/pi/Desktop/Images
+
+sudo python Finexus_Method\ \(RPi\).py &
+sleep 30
+sudo kill $(pgrep -f 'python Finexus_Method\ \(RPi\).py')
+
+sudo python Finexus_Method\ \(RPi\).py &
+sleep 30
+sudo kill $(pgrep -f 'python Finexus_Method\ \(RPi\).py')
+
+sudo python Finexus_Method\ \(RPi\).py &
+
+cd /home/pi/
