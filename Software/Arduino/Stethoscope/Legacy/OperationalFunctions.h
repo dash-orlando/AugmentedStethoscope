244b8438c4205fef021414c789623f18f975d9dc
fluviolobo <fluvio.lobo@gmail.com>
2018-07-31 11:45:37 -0400

    Moved the operational functions header file to the legacy folder instead of deleting it.

---
+++ Software/Arduino/Stethoscope/Legacy/OperationalFunctions.h
@@ -0,0 +1,11 @@
+/*
+ * OperationalFunctions.h
+ *
+ * The following script comprises all of the functions used for the general operation of the smart device
+ *
+ * Michael Xynidis
+ * Fluvio Lobo Fenoglietto
+ * 10/26/2016
+ */
+
+
